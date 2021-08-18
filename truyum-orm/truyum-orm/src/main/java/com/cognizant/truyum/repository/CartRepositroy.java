eecdba6a02af1a2ed2abb462a38853cdd5ef6e38
sb12344321 <88821844+sb12344321@users.noreply.github.com>
2021-08-18 19:10:52 +0530

    Delete truyum-orm/truyum-orm directory

--- truyum-orm/truyum-orm/src/main/java/com/cognizant/truyum/repository/CartRepositroy.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.truyum.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.truyum.model.Cart;
-
-public interface CartRepositroy extends JpaRepository<Cart, Integer> {
-
-}
